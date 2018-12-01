6ced073e8ca5ef8f356923cca00bd66ac0e00453
lubosch <lubomir.vnenk@zoho.com>
2018-12-02 00:33:25 +0100

    Update ruby, set staging env

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:2.3.1
+FROM ruby:2.5.1
 
 RUN curl -sS http://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 RUN echo "deb http://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,13 +1,13 @@
 # Simple Role Syntax
-role :app, %w{swips@13.94.252.19}
-role :web, %w{swips@13.94.252.19}
-role :db,  %w{swips@13.94.252.19}, :primary => true
-role :log, %w{swips@13.94.252.19}
+role :app, %w{swips@40.115.61.51}
+role :web, %w{swips@40.115.61.51}
+role :db,  %w{swips@40.115.61.51}, :primary => true
+role :log, %w{swips@40.115.61.51}
 
 
 set :branch, 'develop'
 set :deploy_to, '/opt/swips_pdfs_printer_staging'
-set :application_port, '3019'
+set :application_port, '3018'
 set :stage, :staging
 set :app_name, 'swips_pdfs_printer_staging'
 
@@ -19,10 +19,10 @@ set :tmp_dir, '/tmp/swips_pdfs_printer_staging'
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '13.94.252.19', user: 'swips', roles: %w{web app db log}#, my_property: :my_value
+server '40.115.61.51', user: 'swips', roles: %w{web app db log}#, my_property: :my_value
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 # set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
